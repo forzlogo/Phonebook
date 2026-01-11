32423f3f048f543d5bbef1e9744f6cd5af62c373
forzlogo <131456994+forzlogo@users.noreply.github.com>
2026-01-12 00:03:42 +0500

    Create validation.h

---
+++ validation.h
@@ -0,0 +1,10 @@
+#ifndef PHONEBOOK_VALIDATION_H_
+#define PHONEBOOK_VALIDATION_H_
+
+#include <string>
+
+bool IsValidEmail(const std::string& email);
+
+bool IsValidPhone(const std::string& phone);
+
+#endif
