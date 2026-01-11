e15e9adc67f35b84ad036ef6856e5fb58381538c
forzlogo <131456994+forzlogo@users.noreply.github.com>
2026-01-12 00:00:40 +0500

    Update contact.h

--- contact.h
+++ contact.h
@@ -1,30 +1,23 @@
-﻿#ifndef CONTACT_H
-#define CONTACT_H
+#ifndef PHONEBOOK_CONTACT_H_
+#define PHONEBOOK_CONTACT_H_
+
 #include <string>
-#include <iostream>
 
-class Contact {
-public:
-    Contact();
-    Contact(const std::string& name, const std::string& phone,
-        const std::string& email);
-    std::string GetName() const;
-    std::string GetPhone() const;
-    std::string GetEmail() const;
-    void SetName(const std::string& name);
-    void SetPhone(const std::string& phone);
-    void SetEmail(const std::string& email);
-
-    void Display() const;
-
-    bool ContainsName(const std::string& search_string) const;
-    bool operator<(const Contact& other) const;
-
-    friend std::ostream& operator<<(std::ostream& os, const Contact& contact);
-    friend std::istream& operator>>(std::istream& is, Contact& contact);
-private:
-    std::string name_;
-    std::string phone_;
-    std::string email_;
+struct Contact {
+    std::string name;
+    std::string phone;
+    std::string email;
 };
+
+Contact CreateContact(const std::string& name,
+    const std::string& phone,
+    const std::string& email);
+
+void DisplayContact(const Contact& contact);
+
+bool CompareContactsByName(const Contact& a, const Contact& b);
+
+bool ContactContainsName(const Contact& contact,
+    const std::string& search_string);
+
 #endif
